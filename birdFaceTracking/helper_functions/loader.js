564ee256a421a0e12924a1d763d93274abf80e80
Kellythlemaire <lkelly@outpost31.ca>
2022-08-09 13:29:50 -0700

    added guerilla project items

--- birdFaceTracking/helper_functions/loader.js
+++ birdFaceTracking/helper_functions/loader.js
@@ -1,5 +1,5 @@
 import {GLTFLoader} from "./GLTFLoader.js";
-import * as THREE from "./node_modules/three/build/three.module.js";
+import * as THREE from "./three/build/three.module.js";
 
 //const THREE = window.MINDAR.IMAGE? window.MINDAR.IMAGE.THREE: window.MINDAR.FACE.THREE;
 
